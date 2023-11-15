f79ec57c94c3b1ec1e7e379e5d669a3c417836b7
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2023-11-15 01:55:17 -0300

    Ajuste consulta da api

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import {
   Container,
   HeaderView,
@@ -13,119 +13,124 @@ import { AccordionItem } from "../../components/Accordion";
 
 import { api } from "../../lib/axios";
 
+import RNPickerSelect from "react-native-picker-select";
+
 import SelectDropdown from "react-native-select-dropdown";
 import { useEffect, useState } from "react";
 
 export function Home() {
   const [mesesPagamento, setMesesPagamento] = useState([]);
   const [mesesList, setMesesList] = useState([]);
+  const [isListReady, setIsListReady] = useState(false);
   const [valorPadraoDropDown, setValorPadraoDropDown] = useState(null);
+  const [loading, setLoading] = useState(true);
   const widthAndHeight = 250;
   const series = [2315, 350];
   const sliceColor = ["#4aac59", "#b52f20"];
-
-  const countries = [
-    { label: "01/2023", value: "01-2023" },
-    { label: "02/2023", value: "02-2023" },
-    { label: "03/2023", value: "03-2023" },
-  ];
 
   const handleSelect = (item, index) => {
     console.log("Selecionado:", item, index);
   };
 
-  const ConvertObject = () => {
-    const newList = mesesPagamento.map((item) => ({
-      label: `${item.mes}/${item.ano}`,
-      value: `${item.mes}-${item.ano}`,
-    }));
-    setMesesList(newList);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await api.get("App/1");
+        if (response.status == 200) {
+          setMesesPagamento(response.data);
 
-    if (newList.length > 0) {
-      setValorPadraoDropDown(newList[newList.length - 1]);
-    }
-  };
+          const newList = response.data.map((item) => ({
+            label: `${item.mes}/${item.ano}`,
+            value: `${item.mes}-${item.ano}`,
+          }));
+          setMesesList(newList);
 
-  async function BuscaMeses() {
-    try {
-      const response = await api.get("App/1");
-
-      if (response.status == 200) {
-        setMesesPagamento(response.data);
+          if (newList.length > 0) {
+            setValorPadraoDropDown(newList[0]);
+          }
+          setIsListReady(true);
+        }
+      } catch (error) {
+        console.log("Erro ao chamar API", error);
+      } finally {
+        setLoading(false);
       }
-    } catch (error) {
-      console.log("erro ao chamar api" + error);
     }
-  }
+
+    fetchData();
+  }, []);
 
   useEffect(() => {
-    async function valor() {
-      await BuscaMeses();
-
-      ConvertObject();
+    if (isListReady) {
+      setLoading(false);
     }
-
-    valor();
-  }, []);
+  }, [isListReady]);
 
   return (
     <Container showsVerticalScrollIndicator={false}>
-      <HeaderView>
-        <Text>Selecione um mês</Text>
-        {valorPadraoDropDown && (
-          <SelectDropdown
-            buttonStyle={{
-              backgroundColor: "#f4f4f4",
-              color: "white",
-              width: "80%",
-            }}
-            rowStyle={{ backgroundColor: "lightgray" }}
-            data={mesesList}
-            onSelect={handleSelect}
-            defaultValue={valorPadraoDropDown}
-            buttonTextAfterSelection={(item, index) => item.label}
-            rowTextForSelection={(item, index) => item.label}
-          />
-        )}
-      </HeaderView>
-      <DashContainer>
-        <PieChart
-          widthAndHeight={widthAndHeight}
-          series={series}
-          sliceColor={sliceColor}
-          coverRadius={0.75}
-          coverFill={"#FFF"}
-        />
-        <CenteredTextContainer>
-          <Text>A receber em</Text>
-          <TextPieChart>14/11/2023</TextPieChart>
-        </CenteredTextContainer>
-      </DashContainer>
-      <ResumeContainer>
-        <View
-          style={{
-            borderBottomColor: "#e1e2e1",
-            borderBottomWidth: 1,
-          }}
-        >
-          <AccordionItem title="Proventos" value={2315} type={"Proventos"}>
-            <DetailView>
-              <Text>Salário base</Text>
-              <Text>R$2.300,00</Text>
-            </DetailView>
-            <DetailView>
-              <Text>Adicional</Text>
-              <Text>R$15,00</Text>
-            </DetailView>
-          </AccordionItem>
+      {loading && <ActivityIndicator />}
+      {!loading && (
+        <View>
+          <HeaderView>
+            {isListReady && !loading && (
+              <View>
+                <Text>Selecione um mês</Text>
+                <SelectDropdown
+                  buttonStyle={{
+                    backgroundColor: "#f4f4f4",
+                    color: "white",
+                    width: "80%",
+                  }}
+                  rowStyle={{ backgroundColor: "lightgray" }}
+                  data={mesesList}
+                  onSelect={handleSelect}
+                  defaultValue={valorPadraoDropDown}
+                  buttonTextAfterSelection={(item, index) => item.label}
+                  rowTextForSelection={(item, index) => item.label}
+                />
+              </View>
+            )}
+          </HeaderView>
+          <DashContainer>
+            <PieChart
+              widthAndHeight={widthAndHeight}
+              series={series}
+              sliceColor={sliceColor}
+              coverRadius={0.75}
+              coverFill={"#FFF"}
+            />
+            <CenteredTextContainer>
+              <Text>A receber em</Text>
+              <TextPieChart>14/11/2023</TextPieChart>
+            </CenteredTextContainer>
+          </DashContainer>
+          <ResumeContainer>
+            <View
+              style={{
+                borderBottomColor: "#e1e2e1",
+                borderBottomWidth: 1,
+              }}
+            >
+              <AccordionItem title="Proventos" value={2315} type={"Proventos"}>
+                <DetailView>
+                  <Text>Salário base</Text>
+                  <Text>R$2.300,00</Text>
+                </DetailView>
+                <DetailView>
+                  <Text>Adicional</Text>
+                  <Text>R$15,00</Text>
+                </DetailView>
+              </AccordionItem>
+            </View>
+            <AccordionItem title="Descontos" value={350} type={"Descontos"}>
+              <DetailView>
+                <Text>Desconto</Text>
+                <Text>R$350,00</Text>
+              </DetailView>
+            </AccordionItem>
+          </ResumeContainer>
         </View>
-        <AccordionItem title="Descontos" value={350} type={"Descontos"}>
-          <DetailView>
-            <Text>Desconto</Text>
-            <Text>R$350,00</Text>
-          </DetailView>
-        </AccordionItem>
-      </ResumeContainer>
+      )}
     </Container>
   );
 }
